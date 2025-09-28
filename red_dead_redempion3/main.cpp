91f22374318948429c1cc4992c4d22edcaae09a9
Tadijas1 <ctadeusz14@gmail.com>
2025-09-28 14:42:00 +0200

    strzelanina 1/2

--- red_dead_redempion3/main.cpp
+++ red_dead_redempion3/main.cpp
@@ -2,53 +2,109 @@
 #include "class.h"
 #include <ctime>
 #include <cstdlib>
-#include <windows.h>
+#include <unistd.h>
+#include <cstdlib>
 
 using namespace std;
 
-int zycie=10, sokole_oko=5, wrogowie, trudnosc;
+int zycie=10, sokole_oko=5;
+char a;
 
-void strzelanina(int t,int w)
+void wykonajStrzal(bool &c, int t, int &w);
+
+void czy_oberwales(bool &c)
 {
-    bool czy=true, jeszzce_raz=true;
-    int strzal;
-    cout<<"Nacisnij S aby zobaczyc swoje statystyki (moższ to zrobic w kazdej chwili)"<<endl;
+    //czy cię trafiono
+    if(1==rand()%3+1){
+        cout<<"Zostales trafiony! Masz o jedno zycie mniej!"<<endl;
+        if(zycie-1==0){cout<<"NIE ZYJESZ"<<endl; c=false;}
+        zycie--;
+    }
+}
+
+void strzelanina(int &t,int &w)
+{
+    system("clear");
+    bool czy=true, pierwszyNapis=true, jeszce_raz=true;
     while (czy)
     {
-        if(1==rand()%3+1)
-        {
-            if(zycie-1==0) cout<<"NIE ZYJESZ"<<endl; czy=false;
-            cout<<"Zostales trafiony! Masz o jedno zycie mniej!"<<endl;
+        system("clear");
+        //przypomnienie
+        if(pierwszyNapis){cout<<"Nacisnij S aby zobaczyc swoje statystyki (moższ to zrobic w kazdej chwili)"<<endl; pierwszyNapis=false;}
+        //wybieranie
+        cout<<"Trwa walka!"<<endl;
+        cout<<"1. Strzelasz"<<endl;
+        cin>>a;
 
-        }
-        cout<<"Wybierz liczbe od 1-"<<t<<": ";
-        cin>>strzal;
-
-        int los1 = rand()%t+1;
-        int los2;
-        do
+        switch (a)
         {
-            los2 = rand()%t+1;
-        } while (los1==los2);
-        int los3;
-        do
-        {
-            los3 = rand()%t+1;
-        } while (los3==los1);
-        
-        if(jeszzce_raz)
-        {
-            if(strzal==los1) cout<<"Trafiles w glowe!"<<endl;
-            else if((strzal==los2)||(strzal==los3)) cout<<"Trafiles w cialo! Strzel jeszcze raz aby zabic!"<<endl; jeszzce_raz=false;
-            else cout<<"Nie trafiles"<<endl;
-            Sleep(2000);
-        }
-        else{
-            jeszzce_raz=true;
+        case '1':
+            wykonajStrzal(jeszce_raz, t, w);
+            czy_oberwales(czy);
+        break;
+        case 's':
+            //staty
+            system("clear");
+            cout<<"Życie: "<<zycie<<endl;
+            cout<<"Sokole oko: "<<sokole_oko<<endl;
+            cout<<"Pozostali wrogowie: "<<w<<endl;
+            sleep(2);
+        break;
+        default:
+            cout<<"Coś poszło nie tak"<<endl;
+        break;
         }
     }
+}
+
+void wykonajStrzal(bool &c,int t,int &w)
+{
+    system("clear");
+    char strzal;
+    cout<<"Wybierz liczbe od 1-"<<t<<": ";
+    cin>>strzal;
+
+    //losowanie
+    int los1 = rand()%t+1;
+    int los2;
+    do
+    {
+        los2 = rand()%t+1;
+    } while (los1==los2);
+    int los3;
+    do
+    {
+        los3 = rand()%t+1;
+    } while ((los3==los1) || (los3==los2));
     
-    
+    //staty
+    if(strzal=='s'){
+        system("clear");
+        cout<<"Życie: "<<zycie<<endl;
+        cout<<"Sokole oko: "<<sokole_oko<<endl;
+        cout<<"Pozostali wrogowie: "<<w<<endl;
+        sleep(2);
+    }
+    else{
+        system("clear");
+        //zamiana chara na int'a
+        strzal=strzal - '0';
+        //czy trafil
+        if(c){
+            //nowa osoba
+            if(strzal==los1){cout<<"Trafiles w glowe!"<<endl;}
+            else if((strzal==los2)||(strzal==los3)){cout<<"Trafiles w cialo! Strzel jeszcze raz aby zabic!"<<endl; c=false;}
+            else cout<<"Nie trafiles"<<endl;
+            sleep(2);
+        }
+        else{
+            //ta sama osoba
+            if((strzal==los1)||(strzal==los2)||(strzal==los3)){cout<<"Trafiłeś!"<<endl; w=w-1; c=false;}
+            else cout<<"Nie trafileś!"<<endl;
+            sleep(2);
+        }
+    }
+    return;
 }
 
 void akcja(Przedmiot *x)
@@ -58,17 +114,18 @@ void akcja(Przedmiot *x)
 
 int main()
 {
+    int wrogowie=3, trudnosc=5;
     srand(time(NULL));
 
-    strzelanina(5, 3);
+    strzelanina(trudnosc, wrogowie);
 
-    Jedzenie p1("puszka", 10);
-    Dynamit d1("bomba", 2);
+    // Jedzenie p1("puszka", 10);
+    // Dynamit d1("bomba", 2);
 
-    Przedmiot *wsk;
+    // Przedmiot *wsk;
 
-    akcja(&p1);
-    akcja(&d1);
+    // akcja(&p1);
+    // akcja(&d1);
 
     return 0;
 }
