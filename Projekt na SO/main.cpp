474a24ec39567c268cddd40ebae9a65b27d4ab93
Tadijas1 <ctadeusz14@gmail.com>
2026-03-15 11:03:11 +0100

    Kod "projekt na SO" działa poprawnie
    - Działa poprawnie
    - Trzeba jeszcze dodać liczenie średniej oczekiwania
    - Połowa sukcesu :3

--- Projekt na SO/main.cpp
+++ Projekt na SO/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <windows.h>
+#include <string>
 
 using namespace std;
 
@@ -8,15 +10,18 @@ int ileP;
 class Proces
 {
     public:
+    string nazwa;
     int czasPrzyjscia;
     int czasPrzetwarzania;
 
-    Proces(int czPrzyj = 0, int czPrzet = 0) {
+    Proces(string n="", int czPrzyj = 0, int czPrzet = 0) {
+        nazwa = n;
         czasPrzyjscia = czPrzyj;
         czasPrzetwarzania = czPrzet;
     }
 
     void pokaz() {
+        cout<<nazwa<<": ";
         cout<<czasPrzyjscia<<" ";
         cout<<czasPrzetwarzania<<endl;
         return;
@@ -43,29 +48,67 @@ int main()
         int x, y;
         cout<<"Czas Przyjscia "<<i+1<<" procesu: "; cin>>x;
         cout<<"Czas Przetwarzania "<<i+1<<" procesu: "; cin>>y;
-        procesy.push_back(Proces(x, y));
+        procesy.push_back(Proces(string("P") + to_string(i+1), x, y));
     }
 
-    //Sortowanie przy pomocy czasuPrzetwarzania (BOMBELKOWE)
-    for (int i = 0; i < procesy.size(); i++) {   
-        for(int i = 0; i < procesy.size()-1; i++) {
-            if(procesy[i].czasPrzetwarzania > procesy[i+1].czasPrzetwarzania) {
-                Proces bufor;
-                bufor = procesy[i];
-                procesy[i] = procesy[i+1];
-                procesy[i+1] = bufor;
-            }
-        }
-    }
-    
-
-    //Test
+    //Pokazanie tablicy na początek
     system("cls"); //system("clear");
     for (int i = 0; i < procesy.size(); i++)
     {
-        procesy[i].pokaz();
+        procesy[i].pokaz(); 
     }
-    getchar();getchar();
+
+    while(!procesy.empty())
+    {
+        //Sortowanie przy pomocy czas przyjścia (BOMBELKOWE)
+        for (int i = 0; i < procesy.size(); i++) {   
+            for(int i = 0; i < procesy.size()-1; i++) {
+                if(procesy[i].czasPrzyjscia > procesy[i+1].czasPrzyjscia) {
+                    Proces bufor;
+                    bufor = procesy[i];
+                    procesy[i] = procesy[i+1];
+                    procesy[i+1] = bufor;
+                }
+            }
+        }
+        
+        //Ile jest procesów z zerem
+        bool czy_zero = true;
+        int ileliczb = 0;
+        for (int i = 0; czy_zero == true; i++)
+        {
+            if(procesy[i].czasPrzyjscia == 0) ileliczb++;
+            else czy_zero = false;
+        }
+
+        //Sortowanie przy pomocy czas przetwarzania (BOMBELKOWE)
+        for (int d = 0; d < ileliczb; d++) {   
+            for(int i = 0; i < ileliczb-1; i++) {
+                if(procesy[i].czasPrzetwarzania > procesy[i+1].czasPrzetwarzania) {
+                    Proces bufor;
+                    bufor = procesy[i];
+                    procesy[i] = procesy[i+1];
+                    procesy[i+1] = bufor;
+                }
+            }
+        }
+
+        //JEDEN TIK
+        procesy[0].czasPrzetwarzania--;
+        if(procesy[0].czasPrzetwarzania == 0) procesy.erase(procesy.begin());
+
+        //Zmniejszanie czasuPrzyjścia u każdego procesu
+        for (int i = 0; i < procesy.size(); i++) if(procesy[i].czasPrzyjscia > 0) procesy[i].czasPrzyjscia--;
+
+        //pokazywanie co się zmieniło
+        system("cls"); //system("clear");
+        for (int i = 0; i < procesy.size(); i++)
+        {
+            procesy[i].pokaz(); 
+        }
+
+        Sleep(1000);
+    }
 
     return 0;
 }
